b712f59cd365ebc439594f2276ed2edd0b24d745
João Gabriel <joaogabriel2007hp@gmail.com>
2024-04-11 12:35:18 -0300

    html-css-otimizado
    HTML e CSS otimizado e começando a codificar JS no site.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll(".header-menu ul a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
