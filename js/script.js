d0ca30683e890ee6e3e4ae66d26a18bf95138f45
João Gabriel <joaogabriel2007hp@gmail.com>
2024-04-11 21:10:12 -0300

    parametros-e-perguntas
    Adicionada a função de selecionar o orçamento automaticamente quando clicado em um produto e a função da parte de perguntas e respostas.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do menu
 const links = document.querySelectorAll(".header-menu ul a");
 
 function ativarLink(link) {
@@ -9,3 +10,36 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Parametros de orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(parametro) {
+  const input = document.getElementById(parametro);
+  if (input) {
+    input.checked = true;
+  }
+  console.log(input);
+}
+parametros.forEach(ativarProdutos);
+console.log(parametros);
+
+//Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  resposta.classList.toggle("ativa");
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
